5bd9b70bd2d23d8fd5ff2e56765b7164d929331d
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-11-25 18:06:36 +0000

    Update settings.py with new secretkey

--- blue_turtle/settings.py
+++ blue_turtle/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cqg#g#)$zgu=_5k=bx^r=%w&zz2mv2w)sy_$rr*it*(5u$gnf8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['blueturtle.herokuapp.com', 'localhost']
 
