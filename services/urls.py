e6dd8888965448cc676d6a59f880e05e880e5ca5
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-11-25 19:41:10 +0000

    Added services templates and wire up urls

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.ServicesView.as_view(), name='services'),
+    path('brake/', views.ServicesView.as_view(), name='brake'),
+    path('suspension/', views.ServicesView.as_view(), name='suspension'),
+    path('full/', views.ServicesView.as_view(), name='full'),
+    path('wheel/', views.ServicesView.as_view(), name='wheel'),
+    path('gear/', views.ServicesView.as_view(), name='gear'),
 ]
