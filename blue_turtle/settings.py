7f279c2d7bf6f44995f01f08947fff4183401f19
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-11-28 10:39:27 +0000

    Updated settings.py to remove whitenoise

--- blue_turtle/settings.py
+++ blue_turtle/settings.py
@@ -65,7 +65,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -219,5 +218,3 @@ else:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
